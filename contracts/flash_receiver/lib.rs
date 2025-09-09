02bac8b37aab325ec87f8676f73a6eb85fa6b316
Juan Villarejo Ortiz <juan.villarejo.ortiz@gmail.com>
2025-09-09 14:22:16 +0800

    Add receiver logic

---
+++ contracts/flash_receiver/lib.rs
@@ -0,0 +1,95 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod Receiver {
+
+    pub enum Action {
+        Arbitrage,
+        Other,
+    }
+
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+    #[ink(storage)]
+    pub struct Receiver {
+        /// Stores the receiver lender's AccountId.
+        lender: AccountId,
+        /// Stores the last action performed.
+        action: Action,
+    }
+
+    impl Receiver {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(lender: AccountId) -> Self {
+            Self {
+                lender,
+                action: Action::Arbitrage,
+            }
+        }
+
+        /// Constructor that initializes the `bool` value to `false`.
+        ///
+        /// Constructors can delegate to other constructors.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(Default::default())
+        }
+
+        /// A message that can be called on instantiated contracts.
+        /// This one flips the value of the stored `bool` from `true`
+        /// to `false` and vice versa.
+        #[ink(message)]
+        pub fn on_flash_loan(
+            &mut self,
+            initiator: AccountId,
+            amount: Balance,
+            fee: Balance,
+            data: Vec<u8>,
+        ) -> bool {
+            let caller = Self::env().caller();
+            if caller != self.lender {
+                return false;
+            }
+            if initiator != Self::env().account_id() {
+                return false;
+            }
+            let decoded_action = self
+                .decode_action(data)
+                .map_err(|_| "FlashBorrower: Unable to decode action".to_string())?;
+
+            match decoded_action {
+                Action::Arbitrage => {
+                    // Mock an arbitrage action, this should be an EV+ operation
+                    self.action = Action::Arbitrage;
+                    // Profitable logic would go here
+                    // Emit event
+                    Self::env().emit_event(ActionPerformed {
+                        action: Action::Arbitrage,
+                        amount,
+                        fee,
+                    });
+                }
+                Action::Other => {
+                    // Perform other action
+                    self.action = Action::Other;
+                    Self::env().emit_event(ActionPerformed {
+                        action: Action::Other,
+                        amount,
+                        fee,
+                    });
+                }
+                _ => return false,
+            }
+
+            true
+        }
+
+        /// Decodes the data into an action
+        #[ink(message)]
+        pub fn decode_action(&self, data: Vec<u8>) -> Result<Action, scale::Error> {
+            Action::decode(&mut &data[..])
+        }
+    }
+}
