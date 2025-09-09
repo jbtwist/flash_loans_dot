9b394bc28ae3ce7708d751feb61b6cb5cfcd9c34
rodrigo ramos xochiteotzin <rodrx20@gmail.com>
2025-09-09 14:55:29 +0800

    updating cargo toml w workspace, adding self to fn

--- contracts/flash_mint/lib.rs
+++ contracts/flash_mint/lib.rs
@@ -8,22 +8,45 @@ mod flash_mint_contract {
         pub callback_success: bool,
     }
 
+    #[ink::trait_definition]
+    fn total_supply() -> u128 {}
+    #[ink::trait_definition]
+    fn allowance(owner: AccountId, spender: AccountId) -> u128 {}
+    #[ink::trait_definition]
+    fn _approve(owner: AccountId, spender: AccountId, amount: u128) {}
+    #[ink::trait_definition]
+    fn _burn(account: AccountId, amount: u128) {}
+    #[ink::trait_definition]
+    fn _mint(account: AccountId, amount: u128) {}
+
+    #[derive(Debug, PartialEq, Eq)]
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
+    pub enum Error {
+
+    }
+
+    pub type Result<T> = core::result::Result<T, Error>;
+
     impl Mint {
-        #[ink(constructor)]
         pub fn new(_fee: u128) -> Self {
             Self {
                 fee: _fee
             }
         }
 
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(5) // default fee is 0.05%
+        }
+
         #[ink(message)]
-        fn max_flash_loan(token: AccountId) -> u128 {
-            // TODO totalSupply come from the ERC20 token standard
-            // return type(uint256).max - totalSupply();
+        fn max_flash_loan(&self, token: AccountId) -> Result<u128> {
+            return U128.MAX - Self::total_supply();
         }
 
         #[ink(message)]
         fn flash_fee(
+            &self,
             token: AccountId,
             amount: u128,
         ) -> u128 {
@@ -35,41 +58,40 @@ mod flash_mint_contract {
         }
 
         fn _flash_fee(
+            &self,
             token: AddressId,
             amount: u128
-        ) -> u128 {
+        ) -> Result<u128> {
             return amount * fee / 10000;
         }
 
         #[ink(message)]
         fn flash_loan(
+            &self,
             receiver: AccountId,
             token: AccountId,
             amount: u128,
             data: Vec<u8>,
-        ) -> bool {
+        ) -> Result<bool> {
             ensure!(
                 token == Self::env::address(),
                 "FlashMinter: Unsupported currency"
             );
 
             let fee: u128 = Self::_flash_fee(token, amount);
-            // TODO mint come from the ERC20 token standard
-            // _mint(receiver, amount);
+            Self::_mint(receiver, amount);
             ensure!(
                 receiver.onFlashLoan(Self::env::caller(), token, amount, fee, data) == callback_success,
                 "FlashMinter: Callback failed"
             );
-            // TODO allowance come from the ERC20 token standard
-            let _allowance: u128 = allowance(receiver, Self::env::address());
+            let _allowance: u128 = Self::allowance(receiver, Self::env::address());
             ensure!(
                 _allowance >= (amount + fee),
                 "FlashMinter: Repay not approved"
             );
-            // TODO _approve and _burn come from the ERC20 token standard
-            _approve(receiver, Self::env::address(), _allowance - (amount + fee));
-            _burn(receiver, amount + fee);
-            return true;
+            self::_approve(receiver, self::env::address(), _allowance - (amount + fee));
+            self::_burn(receiver, amount + fee);
+            return Ok(true);
         }
     }
 }
